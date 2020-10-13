4ad0dc76a3b10d166e1606d1d5c920b9975f7a1f
Charline <charlinerlach@gmail.com>
2020-10-13 18:03:09 -0500

    Added files and updated index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const layout = require("./node_modules/layout");
+
+const questions = [
+    {
+        type: "input",
+        name: "userInput",
+        message: "What is your username on GitHub?"
+    },{
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },{
+        type: "input",
+        name: "description",
+        message: "Describe your repository/project."
+    },{
+        type: "input",
+        name: "installation",
+        message: "Provide installation instructions (step-by-step), if applicable."
+    },{
+        type: "input",
+        name: "usage",
+        message: "Provide usage instructions and examples for use."
+    },{
+        type: "input",
+        name: "userInput",
+        message: "What is your user input (GitHub)?"
+    },{
+        type: "input",
+        name: "contributors",
+        message: "Provide the names of any contributors, if applicable."
+    },{
+        type: "input",
+        name: "test",
+        message: "If applicable, provide any tests completed for the application."
+    },{
+        type: "list",
+        name: "license",
+        message: "Does your project have any of the following applicable licenses?",
+        choices: ["NONE", "MIT", "GNU GPLv3", "Apache 2.0", "The Unlicense"]
+    },{
+        type: "input",
+        name: "questions",
+        message: "How should users reach out to you if they have additional questions?"
+    }
+]
